c674e4a6b05b002da6ebf9a67b7d0504823cfd32
Lahiru Wijesooriya <lahiruhiranwijesooriyansbm@gmail.com>
2020-10-25 12:09:42 +0530

    Add the database connecton

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+
+mongoose.connect ('mongodb+srv://LahiruH:Lahi#1234@cluster0.emfv7.mongodb.net/<dbname>?retryWrites=true&w=majority',
+{ useNewUrlParser: true },
+() => console.log("connected to db!"))
 
 //Import rutes
 const authRoute = require('./routes/auth');
